932624d8e1848d5e9e5d2b0108ded0bac7cc024b
Hallurs <hallurh@7und.is>
2021-12-07 14:12:37 +0000

    Update CinemaAction.js

--- src/actions/CinemaAction.js
+++ src/actions/CinemaAction.js
@@ -1,10 +1,10 @@
-import Cinema from '../services/CinemaService';
+import CinemaService from '../services/CinemaService';
 import * as constants from '../constants';
 
 export const getCinemaList = () => {
     return async dispatch => {
         try {
-            const CinemaList = await weatherService.getCinemaList();
+            const CinemaList = await CinemaService.getCinemaList();
             dispatch(getCinemaListSuccess(CinemaList));
         } catch (err) {
             // TODO: Should dispatch an error action
