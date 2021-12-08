e947b567fa24c432cf2c36ebfd95d7837dc19e74
Hallurs <hallurh@7und.is>
2021-12-08 10:28:14 +0000

    lagaði apan

---
+++ src/services/cinemaService.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const loadCinemas = async() => {
+    const url = 'https://api.kvikmyndir.is/theaters';
+    var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI2MWFkZTkxY2ExMTQ0MDFmNDU2NWRmOTkiLCJnbG9iYWxhZG1pbiI6ZmFsc2UsImFkbWluIjpmYWxzZSwiYWN0aXZlIjp0cnVlLCJmdWxsbmFtZSI6IkhhbGx1ciBIZXJtYW5uc3NvbiBBc3BhciIsImVtYWlsIjoiaGFsbHVyaEA3dW5kLmlzIiwidXNlcm5hbWUiOiJoYWxsaSIsInBhc3N3b3JkIjoiJDJhJDA4JGI1N1QvUXV2T2hSNkI1Tmx6OWcwSk9lbHhjaElLMGg2Zy9Ebi5IcWpIYlRSbDhUd3NvMmpxIiwiZG9tYWluIjoiaGFsbGkiLCJtZXNzYWdlIjoic2Nob29sIHByb2plY3QsIMO-csOzdW4gc23DoWZvcnJpdGEiLCJpYXQiOjE2Mzg5NDk1MDYsImV4cCI6MTYzOTAzNTkwNn0.d_az9zWGlkoEGUMf9ZQoHt3UPHXlQs5bdIzyZojGFGc";     
+    let config = {
+      headers: {
+        "x-access-token" : token
+      }
+    }
+  
+    return await axios.get(url, config);
+    
+};
+
+export default loadCinemas;
